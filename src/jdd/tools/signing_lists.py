a95e1d9fd37b2179774794413f50247e9c21fdec
Quentin <quentin.haenn@ensma.fr>
2023-07-17 14:48:27 +0200

    signing.py created

---
+++ src/jdd/tools/signing_lists.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+"""
+    Prepare jdd emargement list
+"""
+import pandas as pd
+
+def create_file(input_file, output_file) :
+    print("Formatting data ... \n")
+    if '.csv' in input_file :
+        df = pd.read_csv(input_file)
+    else :
+        raise IOError('File extension not supported. Must be .csv')
+    df = df.sort_values(by='Nom')
+    df['Nom'] = df['Nom'].apply(lambda x: x.title().strip())
+    df['Prénom'] = df['Prénom'].apply(lambda x: x.title().strip())
+    last_names = df['Nom'].to_list()
+    first_names = df['Prénom'].to_list()
+    list_combined = tuple(zip(last_names, first_names))
+    print('Writing tex file .....')
+    with open(output_file,'w') as file :
+        for item in list_combined :
+            file.write(r"\Large\textsc{%s} & \Large %s &  &  &  \\ \hline" % item)
+            file.write("\n")
+        print('Done')
+    print("File created under %s" %output_file)
